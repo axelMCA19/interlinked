89c37a025e9f6ce6163e444382add7690a50071e
Axelius <1axelitomca@gmail.com>
2024-12-23 22:52:57 -0400

    v 1.0
    este es el sistema desarrollado para los taxistas

---
+++ sistema/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$direccionservidor="localhost";
+$baseDatos="sistema";
+$usuarioBD= "root";
+$contraseniaBD="";
+
+
+
+// $direccionservidor="localhost";
+// $baseDatos="u409075324_sistema";
+// $usuarioBD= "u409075324_interconectado";
+// $contraseniaBD="A1@2-z.3.2";
+
+
+?>
